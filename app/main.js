77fb80794adaaf054c1b0cd53f95e6f78bae9b0b
Rok <roky.zz@gmail.com>
2017-04-10 12:16:40 +0200

    intial commit

---
+++ app/main.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+class HOC extends React.Component {
+    constructor() {
+        super();
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div></div>
+            </div>
+        );
+    }
+}
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {      
+    ReactDOM.render(<Container/>, document.getElementById('root'));
+}); 
