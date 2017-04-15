49584fd528261da9309cc292838967a6d5d4e5ff
Rok Zigon <roky.dev@evil.si>
2017-04-15 22:54:08 +0200

    corrected errors

--- app/main.js
+++ app/main.js
@@ -7,24 +7,19 @@ var headerComponent = function(props) {
     );
 };
 
-var TextComponent = function(props) {
-    var boxStyle = {height: '10em', padding: '0', backgrondColor: 'lightgrey', border: '1px solid blue', borderRadius: '15px'};
-    return (
-        React.createElement('div', {className: 'boxContent col-xs-12', style: boxStyle}, props.content)
-    );
+var TextComponent = function(props) {    
+    return React.createElement('div', {className: 'boxContent col-xs-12'}, props.content);
 };
 
 
 var BoxContainer = function(props) {
     // TODO: extract content from state object!
     var header = props.header,
-        content = props.content;
-
-     
-         
-         contentStyle = {},
-         bottomLineStyle = {width: '10px', transform: 'rotate(90deg)', borderTopColor: 'blue'},
-         signStyle = {};
+        content = props.content,
+        boxStyle = {height: '10em', padding: '0', backgrondColor: 'lightgrey', border: '1px solid blue', borderRadius: '15px'},
+        contentStyle = {},
+        bottomLineStyle = {width: '10px', transform: 'rotate(90deg)', borderTopColor: 'blue'},
+        signStyle = {};
 
     return (
             React.createElement('div', {className: 'row'}, null,
@@ -48,9 +43,10 @@ var BoxContainer = function(props) {
 
 
 class HOC extends React.Component {
-    constructor() {
-        super();
-        this.state = {tree: {}};
+    constructor(props) {
+        super(props);
+        // TODO: populate state object with all the data needed for a tree
+        this.state = {tree: {header: "Hello", content: "- Yello!"}};
     }
     
     // TODO: create array.map style of rendering
@@ -58,16 +54,13 @@ class HOC extends React.Component {
     render() {
        
         return (
-            React.createElement(BoxContainer, {}, null)
+            React.createElement(BoxContainer, {header: this.state.tree.header, content: this.state.tree.content}, null)
         );
     }
 }
 
 
-
-
-
-var context = {};
+var context = {header: "Hello", content: "- Yello!"};
 
 document.addEventListener("DOMContentLoaded", function(event) {
     ReactDOM.render(React.createElement(HOC, context, null), document.getElementById('root'));
