cc896ca4a533198d9ad91774d7427a44af72da7e
Rok Zigon <roky.dev@evil.si>
2017-04-30 02:39:23 +0200

    added line component

--- app/main.js
+++ app/main.js
@@ -1,10 +1,19 @@
 'use strict'
 
+const lineComponent = function(props) {
+    const lineStyle = {width: props.data.w, top: props.data.top, left: props.data.left,  msTransform: "rotate(" + props.data.deg + "deg)", 
+                        WebkitTransform: "rotate(" + props.data.deg + "deg)", transform: "rotate(" + props.data.deg + "deg)"};
+
+    return (
+        React.createElement('hr', {key: props.data.id, id: props.data.id, className: 'line', style: lineStyle}, null)
+    );
+}
+
 const headerComponent = function(props) {
-    var headerStyle = {backgroundColor: 'blue', borderBottom: '1px', 
+    const headerStyle = {backgroundColor: 'blue', borderBottom: '1px', 
                            borderBottomColor: 'cyan', color: 'white'};
     return (
-        React.createElement('div', {className: props.className, onClick: props.lineConnect, onMouseDown: props.mouseDown, onMouseUpCapture: props.mouseUp, style: headerStyle}, props.header)
+        React.createElement('div', {className: props.className, onClick: props.lineConnect, style: headerStyle}, props.header)
     );
 };
 
@@ -15,7 +24,7 @@ const TextComponent = function(props) {
 
 const BoxContainer = function(props) {
     // TODO: extract content from state object!
-    var header = props.boxElement.header,
+    const header = props.boxElement.header,
         content = props.boxElement.content,
         boxStyle = {top: props.boxElement.position.y, left: props.boxElement.position.x, width: props.boxElement.size.w, height: props.boxElement.size.h};     
 
@@ -24,9 +33,7 @@ const BoxContainer = function(props) {
                                         onDragOver: props.dragOver,
                                         onMouseUp: props.resize},
                 // HEADER, TEXT AND LINE COMPONENT!
-                React.createElement(headerComponent, {header: header, className: 'header col-xs-12', mouseUp: props.mouseUp, 
-                                                                                                     mouseDown: props.mouseDown, 
-                                                                                                     lineConnect: props.lineConnect}, null),
+                React.createElement(headerComponent, {header: header, className: 'header col-xs-12', lineConnect: props.lineConnect}, null),
                 React.createElement('div', {className: 'contentCont'},
                     React.createElement(TextComponent, {content: content, className: 'content col-xs-12'}, null)),                  
                 React.createElement('div', {className: 'addHr', onDragStart: props.mouseUp, onClick: props.lineStart, title: 'Click to connect boxes!'},'+')
@@ -43,7 +50,6 @@ class HOC extends React.Component {
         this.dragElementId = -1;
         this.dragElement = null;
         this.lineElement = null;
-        this.upElement = null;
         this.position = {};
         this.state = {tree: this.props.elementsObject};
         // for box dragging event listeners - but no need if not using "this" keyword inside
@@ -54,19 +60,6 @@ class HOC extends React.Component {
         // for connecting lines
         this.lineStart = this.lineStart.bind(this);
         this.lineConnect = this.lineConnect.bind(this);
-        this.mouseUp = this.mouseUp.bind(this);
-        this.mouseDown = this.mouseDown.bind(this);
-    }
-
-    mouseDown(e) {
-        e = e || window.event;
-        e.currentTarget.style.cursor = 'grabbing';
-        this.upElement = e.currentTarget;
-    }
-    mouseUp(e) {
-        e = e || window.event;        
-        e.currentTarget.style.cursor = 'grab';
-        this.upElement = null;
     }
 
     changeBoxSize(e) {
@@ -111,9 +104,7 @@ class HOC extends React.Component {
               size = this.state.tree[id].size,
               newPosition = {};
         
-        this.upElement == null ? void 0 : (this.upElement.style.cursor = 'grab');
         this.dragElement = null;
-        this.upElement = null;
         newPosition[id] = {header, content, expanded, position: {x: this.position.x, y: this.position.y}, size};
         this.setState({tree: Object.assign({}, this.state.tree, newPosition)});
  
@@ -154,14 +145,20 @@ class HOC extends React.Component {
         (() => {
             for (const key in this.state.tree) {
                  const boxElement = this.state.tree[key];
-                 elementsArr.push(React.createElement(BoxContainer, {key: key, id: key + '_', boxElement, 
+                 elementsArr.push(React.createElement(BoxContainer, {key: key, id: key + '_', 
+                                                                        boxElement: boxElement, 
                                                                         dragStart: this.dragStart,
                                                                         dragOver: this.dragOver, 
                                                                         resize: this.changeBoxSize,
                                                                         lineStart: this.lineStart,
-                                                                        lineConnect: this.lineConnect,
-                                                                        mouseDown: this.mouseDown,
-                                                                        mouseUp: this.mouseUp}, null));
+                                                                        lineConnect: this.lineConnect}, null));
+                if (boxElement.lines == null) {
+                    continue;
+                } else {
+                    for (const lineId in boxElement.lines) {
+                        elementsArr.push(React.createElement(lineComponent, {id: lineId, data: boxElement.lines[lineId]}, null));
+                    }
+                }
             }
         })();
 
@@ -172,35 +169,43 @@ class HOC extends React.Component {
 var elementsObject = {
     0: {
         id: 0,
+        parent: -1,
         header:"header1", 
         content: "option",
         expanded: true,
-        position: {x: '10px', y: '10px'},
-        size: {w: '100px', h: '100px'}
+        position: {x: '610px', y: '10px'},
+        size: {w: '100px', h: '100px'},
+        lines: {1000: {top: '110px', left: '660px', w: '100px', deg: 45}}
      },
      1: {
         id: 1,
+        parent: -1,
         header:"row 1", 
         content: "option 1",
         expanded: true,
-        position: {x: '30px', y: '30px'},
-        size: {w: '100px', h: '100px'}
+        position: {x: '470px', y: '110px'},
+        size: {w: '100px', h: '100px'},
+        lines: null
      }, 
      2: { 
         id: 2,
+        parent: -1,
         header:"row 1", 
         content: "option 2",
         expanded: true,
-        position: {x: '60px', y: '60px'},
-        size: {w: '100px', h: '100px'}
+        position: {x: '360px', y: '260px'},
+        size: {w: '100px', h: '100px'},
+        lines: null
      }, 
      3: { 
         id: 3,
+        parent: -1,
         header:"row 1", 
         content: 'option 3',
         expanded: true,
-        position: {x: '100px', y: '100px'},
-        size: {w: '100px', h: '100px'}
+        position: {x: '260px', y: '260px'},
+        size: {w: '100px', h: '100px'},
+        lines: null
      }
 };
 
